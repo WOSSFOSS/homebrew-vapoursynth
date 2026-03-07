240275f85935ef9445cb37386c0d20a90c9a9424
WOSSFOSS <231392499+WOSSFOSS@users.noreply.github.com>
2026-03-07 18:44:45 -0500

    vapoursynth-wwxd 1.0 (new formula)

---
+++ Formula/vapoursynth-wwxd.rb
@@ -0,0 +1,28 @@
+class VapoursynthWwxd < Formula
+  desc "Xvid-like scene change detection for VapourSynth"
+  homepage "https://github.com/dubhater/vapoursynth-wwxd"
+  url "https://github.com/dubhater/vapoursynth-wwxd/archive/refs/tags/v1.0.tar.gz"
+  sha256 "29da62c3cde87bd4ebb7ceaf7ad8344834412fcdddbc9d115ed62f478c42e874"
+  license ""
+  head "https://github.com/dubhater/vapoursynth-wwxd.git", branch: "master"
+
+  depends_on "pkgconf" => :build
+  depends_on "vapoursynth"
+
+  def install
+    install_name = OS.mac? ? "libwwxd.dylib" : "libwwxd.so"
+
+    system ENV.cc, "-o", install_name, "-fPIC", "-shared", "-O2", "-Wall", "-Wextra", "-Wno-unused-parameter",
+           *`pkg-config --cflags vapoursynth`.split, "src/wwxd.c", "src/detection.c"
+    mkdir "#{lib}/vapoursynth"
+    lib.install install_name => "vapoursynth/#{install_name}"
+  end
+
+  test do
+    python = Formula["vapoursynth"].deps
+                                   .find { |d| d.name.match?(/^python@\d\.\d+$/) }
+                                   .to_formula
+                                   .opt_libexec/"bin/python"
+    system python, "-c", "from vapoursynth import core; core.wwxd"
+  end
+end
