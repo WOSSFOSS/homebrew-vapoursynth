d6f30573cd20773b830f4c20e3bf3df1b0016cbd
WOSSFOSS <231392499+WOSSFOSS@users.noreply.github.com>
2026-03-15 22:02:27 +0000

    vs-nlm-ispc: add 2 bottle.

--- Formula/vs-nlm-ispc.rb
+++ Formula/vs-nlm-ispc.rb
@@ -6,6 +6,14 @@ class VsNlmIspc < Formula
   license "GPL-3.0-or-later"
   head "https://github.com/AmusementClub/vs-nlm-ispc.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/WOSSFOSS/homebrew-vapoursynth/releases/download/vs-nlm-ispc-2"
+    sha256 cellar: :any,                 arm64_tahoe:   "6fc5a7f04b1190de92f2db8dd2d7bbf34cd9e9467546e77bc6921ebe572b199e"
+    sha256 cellar: :any,                 arm64_sequoia: "e41d106da7def19d695d207428f80068b2d5b8d2eb0111c2f8c724d7f6e08094"
+    sha256 cellar: :any,                 arm64_sonoma:  "399e7f123d3a3f1f513e1094d0e4bc95784582ef8c8fd22db1730cddd1283529"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c60fd100b08d6844dce8948bc75f41912c1cb0eee44c97f92871f0fc2105f827"
+  end
+
   depends_on "cmake" => :build
   depends_on "ispc" => :build
   depends_on "pkgconf" => :build
