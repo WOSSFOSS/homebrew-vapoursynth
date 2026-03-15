ea01861a7e40651b778100c1a405a9c8a08aa561
WOSSFOSS <231392499+WOSSFOSS@users.noreply.github.com>
2026-03-15 02:40:21 -0400

    Fix crashing issue on ubuntu and sequoia

--- Formula/vapoursynth-placebo.rb
+++ Formula/vapoursynth-placebo.rb
@@ -41,18 +41,28 @@ class VapoursynthPlacebo < Formula
                                    .find { |d| d.name.match?(/^python@\d\.\d+$/) }
                                    .to_formula
                                    .opt_libexec/"bin/python"
-    (testpath/"test.py").write <<~PYTHON
-      from vapoursynth import core
-      import sys
-      clip = core.std.BlankClip(length=5, width=1920, height=1080, fpsnum=24, fpsden=1)
-      clip = core.placebo.Deband(clip)
-      clip.output(sys.stdout)
-    PYTHON
-    python_call = "#{python} test.py"
-    x265_call = "#{Formula["x265"].opt_bin}/x265 - --input-res 1920x1080 --fps 24 --output test.hevc"
-    call = "#{python_call} | #{x265_call}"
-    system "sh", "-c", "#{python_call} > /dev/null"
-    system "sh", "-c", call
-    assert_path_exists testpath/"test.hevc"
+    if OS.linux?
+      # This is the only safe test because some Akarin features require Vulkan support,
+      # and Vulkan support on CI is very spotty.
+      system python, "-c", "from vapoursynth import core; core.akarin"
+    else
+      if OS.mac? && MacOS.version <= :sequoia
+        # https://github.com/KhronosGroup/MoltenVK/issues/2373
+        ENV["MVK_CONFIG_USE_METAL_ARGUMENT_BUFFERS"] = "0"
+      end
+      (testpath/"test.py").write <<~PYTHON
+        from vapoursynth import core
+        import sys
+        clip = core.std.BlankClip(length=5, width=1920, height=1080, fpsnum=24, fpsden=1)
+        clip = core.placebo.Deband(clip)
+        clip.output(sys.stdout)
+      PYTHON
+      python_call = "#{python} test.py"
+      x265_call = "#{Formula["x265"].opt_bin}/x265 - --input-res 1920x1080 --fps 24 --output test.hevc"
+      call = "#{python_call} | #{x265_call}"
+      system "sh", "-c", "#{python_call} > /dev/null"
+      system "sh", "-c", call
+      assert_path_exists testpath/"test.hevc"
+    end
   end
 end
