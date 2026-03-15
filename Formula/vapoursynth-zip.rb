a5227adff24708b5696656633981f8d3ac63ab2e
WOSSFOSS <231392499+WOSSFOSS@users.noreply.github.com>
2026-03-15 08:05:20 +0000

    vapoursynth-zip: add 13 bottle.

--- Formula/vapoursynth-zip.rb
+++ Formula/vapoursynth-zip.rb
@@ -5,6 +5,14 @@ class VapoursynthZip < Formula
   sha256 "d8916f1d04fae4123a48c53e37b15ca015cb4a8a4f6e43e75c870ac2524dd0e0"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/WOSSFOSS/homebrew-vapoursynth/releases/download/vapoursynth-zip-13"
+    sha256 cellar: :any,                 arm64_tahoe:   "c97ff244ff096b9a9cd1a513d83f42d2fe99bed060b5689f4ec1cac22e3e94d5"
+    sha256 cellar: :any,                 arm64_sequoia: "a41ee1e1d8e2cb27ec7e9ea6770fa9ea5de9ba6b80ccc90b68619458b8dc7158"
+    sha256 cellar: :any,                 arm64_sonoma:  "f589de1b2c35da49fefeeff766dc46769204418bc78eb4ec59fd1bd49fda5d2c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2104cacda12aee1fb83f8e8d52ec1a216286772dedc370a647cf462cca9749fc"
+  end
+
   depends_on "zig" => :build
   depends_on "x265" => :test
   depends_on "vapoursynth"
